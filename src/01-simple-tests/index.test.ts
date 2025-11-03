8d1b4e71e0a52a3d9e333c1add2499d835e20dd3
Aliaksandr <am@mediacube.io>
2025-11-03 09:37:33 +0700

    fix: lint fixes for 01

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -3,30 +3,40 @@ import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    expect(simpleCalculator({ a: 1, b: 2, action: Action.Add })).toEqual(3)
+    expect(simpleCalculator({ a: 1, b: 2, action: Action.Add })).toEqual(3);
   });
 
   test('should subtract two numbers', () => {
-    expect(simpleCalculator({ a: 1, b: 2, action: Action.Subtract })).toEqual(-1)
+    expect(simpleCalculator({ a: 1, b: 2, action: Action.Subtract })).toEqual(
+      -1,
+    );
   });
 
   test('should multiply two numbers', () => {
-    expect(simpleCalculator({ a: 1, b: 2, action: Action.Multiply })).toEqual(2)
+    expect(simpleCalculator({ a: 1, b: 2, action: Action.Multiply })).toEqual(
+      2,
+    );
   });
 
   test('should divide two numbers', () => {
-    expect(simpleCalculator({ a: 3, b: 2, action: Action.Divide })).toEqual(1.5)
+    expect(simpleCalculator({ a: 3, b: 2, action: Action.Divide })).toEqual(
+      1.5,
+    );
   });
 
   test('should exponentiate two numbers', () => {
-    expect(simpleCalculator({ a: 3, b: 2, action: Action.Exponentiate })).toEqual(9)
+    expect(
+      simpleCalculator({ a: 3, b: 2, action: Action.Exponentiate }),
+    ).toEqual(9);
   });
 
   test('should return null for invalid action', () => {
-    expect(simpleCalculator({ a: 1, b: 2, action: 'asdad' })).toEqual(null)
+    expect(simpleCalculator({ a: 1, b: 2, action: 'asdad' })).toEqual(null);
   });
 
   test('should return null for invalid arguments', () => {
-    expect(simpleCalculator({ a: 's', b: 'a', action: Action.Divide })).toEqual(null)
+    expect(simpleCalculator({ a: 's', b: 'a', action: Action.Divide })).toEqual(
+      null,
+    );
   });
 });
