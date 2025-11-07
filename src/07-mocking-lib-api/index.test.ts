e85131e8df29531660b9307191a3382e128d0b3f
Aliaksandr <am@mediacube.io>
2025-11-07 15:41:38 +0700

    feat: add 07 lib mocking

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,59 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
+jest.mock('lodash', () => ({ throttle: jest.fn((fn) => fn) }));
+
+const axiosMock = {
+  defaults: {},
+  interceptors: {
+    request: { use: jest.fn() },
+    response: { use: jest.fn() },
+  },
+  get: jest.fn(),
+  delete: jest.fn(),
+  head: jest.fn(),
+  options: jest.fn(),
+  post: jest.fn(),
+  put: jest.fn(),
+  patch: jest.fn(),
+  getUri: jest.fn(),
+} as unknown as AxiosInstance;
 
 describe('throttledGetDataFromApi', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue({ data: {} });
+    jest.spyOn(axios, 'create').mockReturnValue({
+      ...axiosMock,
+      get: mockGet,
+    } as unknown as AxiosInstance);
+    await throttledGetDataFromApi('/users');
+    expect(axios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue({ data: {} });
+    jest.spyOn(axios, 'create').mockReturnValue({
+      ...axiosMock,
+      get: mockGet,
+    } as unknown as AxiosInstance);
+    await throttledGetDataFromApi('/users');
+    expect(mockGet).toHaveBeenCalledWith('/users');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const data = { id: 1 };
+    const mockGet = jest.fn().mockResolvedValue({ data });
+    jest.spyOn(axios, 'create').mockReturnValue({
+      ...axiosMock,
+      get: mockGet,
+    } as unknown as AxiosInstance);
+    const daat = await throttledGetDataFromApi('/users');
+    await expect(mockGet()).resolves.toEqual({ data });
+    expect(daat).toEqual(data);
   });
 });
